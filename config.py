d1eb0344eb0de1132ee5bc0c5fbaefb0d27a588d
official-ztx <wtuhin707@gmail.com>
2024-10-27 00:37:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = API_ID
-API_HASH = "API_HASH"
+API_ID = 23028479
+API_HASH = "c1e6a93b04c0810a5c282d8d8d44ea6f"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "7892211587:AAGrn0aYADE75zX9pJOrofw0iaTbTwv1bPk"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "MONGO_DB_URI"
+MONGO_DB_URI = "mongodb+srv://GOKU:MISSBHOPALI@goku.pzzsl8d.mongodb.net/?retryWrites=true&w=majority"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = LOG_GROUP_ID
+LOG_GROUP_ID = -1002252184024
 
 # Get this value from @ultron2_robot on Telegram by /id
-OWNER_ID = OWNER_ID
+OWNER_ID = 7038202445
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = "https://t.me/thanos_pro"
-SUPPORT_GROUP = "https://t.me/thanosprosss"
+SUPPORT_CHANNEL = "https://t.me/Shorekeeper_updates"
+SUPPORT_GROUP = "https://t.me/Anime_Group_India_Chat"
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -63,10 +63,10 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 # Get your pyrogram v2 session from Replit
 STRING1 = "STRING_SESSION"
-STRING2 = getenv("STRING_SESSION2", None)
-STRING3 = getenv("STRING_SESSION3", None)
-STRING4 = getenv("STRING_SESSION4", None)
-STRING5 = getenv("STRING_SESSION5", None)
+STRING2 = getenv("BQGWS8YAcExf2Cp-m9-Z388GFSKeUyXsv-rauB0jNKp6BQRnlxZoflKYQJsUDpk0ii2VAAmduJZ79ncqyuejJyckRijhrWkWzQdbFiz-eH95xLciXrPprMBVDDup95_s81jAjIWJ2zO0_Vvg7RMNIY0weyX36A66oL9KS9GlkZb0tGOFrXJZG1qeio5LsvP1j8adtmGhcA_P6l8CXB8mMU-L_P21bFOIsgqlUTfYuhNPbsjZx-oTKNsr1Mf51XR7pGiYbdUIf9t9dCIsyAg_jKEyoBeTAvfn8UDCp2mnVMgiR8nYRmOZh3sRwrXcrFArpcZD-v49NFm-pUfsj1xZk6L2D77zIwAAAAG-IuuuAA", None)
+STRING3 = getenv("BQGWS8YAcExf2Cp-m9-Z388GFSKeUyXsv-rauB0jNKp6BQRnlxZoflKYQJsUDpk0ii2VAAmduJZ79ncqyuejJyckRijhrWkWzQdbFiz-eH95xLciXrPprMBVDDup95_s81jAjIWJ2zO0_Vvg7RMNIY0weyX36A66oL9KS9GlkZb0tGOFrXJZG1qeio5LsvP1j8adtmGhcA_P6l8CXB8mMU-L_P21bFOIsgqlUTfYuhNPbsjZx-oTKNsr1Mf51XR7pGiYbdUIf9t9dCIsyAg_jKEyoBeTAvfn8UDCp2mnVMgiR8nYRmOZh3sRwrXcrFArpcZD-v49NFm-pUfsj1xZk6L2D77zIwAAAAG-IuuuAA", None)
+STRING4 = getenv("BQGWS8YAcExf2Cp-m9-Z388GFSKeUyXsv-rauB0jNKp6BQRnlxZoflKYQJsUDpk0ii2VAAmduJZ79ncqyuejJyckRijhrWkWzQdbFiz-eH95xLciXrPprMBVDDup95_s81jAjIWJ2zO0_Vvg7RMNIY0weyX36A66oL9KS9GlkZb0tGOFrXJZG1qeio5LsvP1j8adtmGhcA_P6l8CXB8mMU-L_P21bFOIsgqlUTfYuhNPbsjZx-oTKNsr1Mf51XR7pGiYbdUIf9t9dCIsyAg_jKEyoBeTAvfn8UDCp2mnVMgiR8nYRmOZh3sRwrXcrFArpcZD-v49NFm-pUfsj1xZk6L2D77zIwAAAAG-IuuuAA", None)
+STRING5 = getenv("BQGWS8YAcExf2Cp-m9-Z388GFSKeUyXsv-rauB0jNKp6BQRnlxZoflKYQJsUDpk0ii2VAAmduJZ79ncqyuejJyckRijhrWkWzQdbFiz-eH95xLciXrPprMBVDDup95_s81jAjIWJ2zO0_Vvg7RMNIY0weyX36A66oL9KS9GlkZb0tGOFrXJZG1qeio5LsvP1j8adtmGhcA_P6l8CXB8mMU-L_P21bFOIsgqlUTfYuhNPbsjZx-oTKNsr1Mf51XR7pGiYbdUIf9t9dCIsyAg_jKEyoBeTAvfn8UDCp2mnVMgiR8nYRmOZh3sRwrXcrFArpcZD-v49NFm-pUfsj1xZk6L2D77zIwAAAAG-IuuuAA", None)
 
 
 BANNED_USERS = filters.user()
@@ -77,12 +77,12 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+START_IMG_URL = "https://envs.sh/AJT.jpg"
 
-PING_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+PING_IMG_URL = "https://envs.sh/W_z.jpg"
 
 PLAYLIST_IMG_URL = "https://graph.org/file/763a841a2ad5cbb1e2fc5.jpg"
-STATS_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+STATS_IMG_URL = "https://envs.sh/AJA.jpg"
 TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
